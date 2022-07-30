7b8fc443eb1a80f2e2929169f8fb754d89c2ecdd
Chris Toph <crchristopherrivera@gmail.com>
2022-07-29 21:24:34 -0400

    Recreated app with daisyui and tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+    extend: {
+      fontFamily: {
+        fira: ["Fira Code", "monospace"],
+      },
+    },
+  }, plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      "emerald"
+    ]
+  }
 }
