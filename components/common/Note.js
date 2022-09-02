67186671092f3b275df3e51de682f220ef555e41
tophc7 <chris@toph.cc>
2022-09-02 15:59:16 -0400

    Added partial functionality to notes page WIP Notes open future to be Note editor popup Added new note button with Note Editor popup

--- components/common/Note.js
+++ components/common/Note.js
@@ -2,18 +2,41 @@ import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Note(props) {
+  // using a random hopefully unique id for now until we get the ones form the Database
+  //needs a unique id to open the correct modal
+  let id = "note" + Math.floor(Math.random() * 1000);
+
   return (
-    <div className="daisy-border-box group card relative overflow-visible bg-info p-4 text-info-content">
-      <h2 className="card-title">Quiz 2 Question 3</h2>
-      <p>This Questions was very hard besties</p>
+    <>
+      <a
+        href={"#" + id}
+        className="daisy-border-box group card relative gap-1 overflow-visible bg-info p-4 text-info-content"
+      >
+        <h2 className="card-title">{props.title}</h2>
+        <p className="max-w-md">{props.text}</p>
 
-      <div className="absolute bottom-0 right-0 h-6 w-6 overflow-hidden">
-        <div className="traingle-t-l mt-2 ml-2 h-4 w-4" />
+        <div className="absolute bottom-0 right-0 h-6 w-6 overflow-hidden">
+          <div className="traingle-t-l mt-2 ml-2 h-4 w-4" />
+        </div>
+
+        <button className="absolute -left-2 -top-2 flex h-6 w-6 scale-0 place-items-center justify-center rounded-full bg-secondary text-xs text-secondary-content shadow-md transition-all duration-300 group-hover:scale-100">
+          <FontAwesomeIcon icon={faPen} />
+        </button>
+      </a>
+
+      {/* FIXME: this should be a note editor component will refactor */}
+      <div className="modal" id={id}>
+        <div className="modal-box">
+          <i>
+            <b>{id}</b>
+          </i>
+          <div className="modal-action">
+            <a href="#" className="btn">
+              Yay!
+            </a>
+          </div>
+        </div>
       </div>
-
-      <button className="absolute right-4 flex h-6 w-6 scale-0 place-items-center justify-center rounded-full bg-secondary text-xs text-secondary-content shadow-md transition-all duration-300 group-hover:scale-100">
-        <FontAwesomeIcon icon={faPen} />
-      </button>
-    </div>
+    </>
   );
 }
--- pages/javaCourse/notes.js
+++ pages/javaCourse/notes.js
@@ -1,14 +1,54 @@
 import Note from "@/components/common/Note";
 import NoteTextBox from "@/components/common/NoteTextBox";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Notes() {
   return (
     <main className="main-content">
-      <h1 className=" main-header">Notes</h1>
-      <Note />
-      <Note />
-      <Note />
-      <NoteTextBox />
+      <header className="flex gap-8">
+        <h1 className=" main-header mr-auto">Notes</h1>
+
+        {/* FIXME: this label+modal should be a component itslef will refactor it */}
+        <label htmlFor="new-note" className="modal-button btn gap-2">
+          <FontAwesomeIcon icon={faPlus} />
+          New Note
+        </label>
+
+        <input type="checkbox" id="new-note" className="modal-toggle" />
+        <div className="modal">
+          <div className="modal-box flex flex-col gap-6 bg-primary  text-primary-content">
+            <input type="text" placeholder="Title" className="title-input" />
+            <textarea className="note-input" placeholder="Write note here" />
+            <div className="modal-action m-0">
+              <label htmlFor="new-note" className="btn btn-ghost">
+                cancel
+              </label>
+              <label htmlFor="new-note" className="btn btn-accent">
+                Save
+              </label>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div className="flex flex-wrap gap-4">
+        <Note
+          title="I have a beautiful Title"
+          text="this is a lot of text dude"
+        />
+        <Note
+          title="I have a beautiful Title"
+          text="this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude "
+        />
+        <Note
+          title="I have a beautiful Title"
+          text="this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude "
+        />
+        <Note
+          title="I have a beautiful Title"
+          text="this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude this is a lot of text dude "
+        />
+      </div>
     </main>
   );
 }
